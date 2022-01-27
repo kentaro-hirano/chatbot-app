7283144bb72067a76921cbb9b9047e03c9664a9f
kentaro-hirano <yakyukentaro@gmail.com>
2022-01-27 10:48:59 +0900

    custome material ui

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import { createStyles } from "@mui/material";
 
-const useStyles = makeStyles((theme) => ({
-  root: {},
-}));
+const useStyles = makeStyles(() =>
+  createStyles({
+    button: {
+      borderColor: "#ffb549",
+      color: "#ffb549",
+      fontWeight: 600,
+      marginBottom: "8px",
+      "&:hover": {
+        backgroundColor: "#ffb549",
+        color: "#fff",
+      },
+    },
+  })
+);
 
 const Answer = (props) => {
   const classes = useStyles();
   console.log(props);
   return (
     <Button
-      variant="contained"
-      color="primary"
+      className={classes.button}
+      variant="outlined"
       onClick={() => props.select(props.content, props.nextId)}
     >
       {props.content}
